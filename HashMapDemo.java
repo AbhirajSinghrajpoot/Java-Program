cbff9748d7dd8468099c3b9cc6d7c5897ea28097
AbhirajSinghRajpoot <Abhirajsingh2k5@gmail.com>
2026-02-26 11:30:52 +0530

    Refactor HashMapDemo to use Integer keys and values; add tempCodeRunnerFile.java

--- HashMapDemo.java
+++ HashMapDemo.java
@@ -2,12 +2,17 @@ import java.util.HashMap;
 
 public class HashMapDemo {
     public static void main(String[] args) {
-        HashMap<String, Integer> map = new HashMap<>();
-        map.put("One", 1);
-        map.put("Two", 2);
-        map.put("Three", 3);
+        HashMap<Integer, Integer> map = new HashMap<>();
+        map.put(1, 10);
+        map.put(2, 23);
+        map.put(3, 28);
         System.out.println(map);
-        System.out.println(map.get("Two"));
+        System.out.println(map.get(2));
+        map.remove(1);
+        System.out.println(map);
+        System.out.println(map.containsKey(3));
+        System.out.println(map.containsValue(20));
+
         
     
     }
