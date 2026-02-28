7dbb1b81a280dad747552495d69bbfd4265fd8b7
AbhirajSinghRajpoot <Abhirajsingh2k5@gmail.com>
2026-02-28 23:46:28 +0530

    Refactor HashMapDemo.java to uncomment and enable character frequency counting functionality

--- HashMapDemo.java
+++ HashMapDemo.java
@@ -34,17 +34,17 @@ public class HashMapDemo {
 
         }
 
-        // HashMap<Character, Integer> map = new HashMap<>();
-        // for (int i = 0; i <str.length(); i++) {
-        //     char ch = str.charAt(i);
-        //     map.put(ch, map.getOrDefault(ch, 0) + 1);
-        // }  
-        // for (char ch : str.toCharArray()) {
-        //     if(map.get(ch) == 1) {
-        //         System.out.println(ch);
-        //         break;
-        //     }
-        // System.out.println(map);
-        // }
+        HashMap<Character, Integer> map = new HashMap<>();
+        for (int i = 0; i <str.length(); i++) {
+            char ch = str.charAt(i);
+            map.put(ch, map.getOrDefault(ch, 0) + 1);
+        }  
+        for (char ch : str.toCharArray()) {
+            if(map.get(ch) == 1) {
+                System.out.println(ch);
+                break;
+            }
+        System.out.println(map);
+        }
     }
 }
