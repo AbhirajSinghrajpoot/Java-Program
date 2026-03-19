438309794ef7638b2c04b86a6b2bf7077aee7663
AbhirajSinghRajpoot <Abhirajsingh2k5@gmail.com>
2026-03-19 21:06:38 +0530

    Add AverageOfArray program with input validation

---
+++ AverageOfArray.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class AverageOfArray {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter the number of elements: ");
+        int n = scanner.nextInt();
+
+        if (n <= 0) {
+            System.out.println("Please enter a positive number of elements.");
+            scanner.close();
+            return;
+        }
+
+        int[] arr = new int[n];
+        int sum = 0;
+
+        System.out.println("Enter " + n + " elements:");
+        for (int i = 0; i < n; i++) {
+            arr[i] = scanner.nextInt();
+            sum += arr[i];
+        }
+
+        double average = (double) sum / n;
+        System.out.printf("Average: %.2f%n", average);
+
+        scanner.close();
+    }
+}
